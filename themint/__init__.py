ddf6a8a02ff0df3ea75bb99f194b086edea6bc7e
tsangsimon <simon.tsang@gmail.com>
2014-07-30 16:00:01 +0100

    adding Sentry configuration

--- themint/__init__.py
+++ themint/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 import logging
 from flask.ext.basicauth import BasicAuth
 from flask.ext.pushrod import Pushrod
+from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 Pushrod(app)
@@ -13,8 +14,10 @@ if not app.debug:
     app.logger.addHandler(logging.StreamHandler())
     app.logger.setLevel(logging.INFO)
 
+if 'SENTRY_DSN' in os.environ:
+    sentry = Sentry(app, dsn=os.environ['SENTRY_DSN'])
+
 app.logger.info( "============")
 app.logger.info(app.config)
 app.logger.debug(app.debug)
 app.logger.info("============")
-
