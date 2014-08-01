796f10e168afd4de07851020eee3d756383fcafc
Adam Shimali <adam.shimali@gmail.com>
2014-08-01 10:37:35 +0100

    Move env vars to environment and environment_test

--- config.py
+++ config.py
@@ -2,18 +2,15 @@ import os
 
 class Config(object):
     DEBUG = False
-    SYSTEMOFRECORD_URL = os.environ.get('SYSTEMOFRECORD_URL')
-    REDIS_URL = os.environ.get('REDIS_URL')
-    REDIS_NS_QUEUE_MINT = os.environ.get('REDIS_NS_QUEUE_MINT')
+    SYSTEMOFRECORD_URL = os.environ['SYSTEMOFRECORD_URL']
+    REDIS_URL = os.environ['REDIS_URL']
+    REDIS_NS_QUEUE_MINT = os.environ['REDIS_NS_QUEUE_MINT']
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
 class TestConfig(DevelopmentConfig):
     TESTING = True
-    SYSTEMOFRECORD_URL = ''
-    REDIS_URL = ''
-    REDIS_NS_QUEUE_MINT = ''
 
 class DockerConfig(Config):
     DEBUG = True
--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-export SETTINGS='config.DevelopmentConfig'
-export SYSTEMOFRECORD_URL=http://0.0.0.0:8000
-export REDIS_URL="redis://user:@localhost:6379"
-export REDIS_NS_QUEUE_MINT="lr:queue:mint:"
-
 source ./environment.sh
 python run_dev.py
