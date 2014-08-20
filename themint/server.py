07ee7ea27ca8da151a22c638e47886210165f399
Juan Matthys Uys <opyate@gmail.com>
2014-08-20 13:10:58 +0100

    return json response

--- themint/server.py
+++ themint/server.py
@@ -2,17 +2,26 @@ from flask import request, make_response
 from .mint import Mint
 from themint import app
 from .health import Health
+import json
 
 mint = Mint()
 Health(app, checks=[mint.health])
+
 
 @app.route('/', methods=['GET'])
 def index():
     return "Mint OK"
 
+
 @app.route('/titles/<title_number>', methods=['POST'])
 def post(title_number):
-    app.logger.info("Recieved title number %s to mint new record with json %s" % (title_number, request.json))
+    app.logger.info(
+            "Recieved title number %s to mint new record with json %s" %
+            (title_number, request.json))
     r = mint.create(request.json)
     app.logger.info("Response status code %s" % r.status_code)
-    return make_response(r.text, r.status_code)
+    return make_response(
+            json.dumps({
+                'message': r.text,
+                'status_code': r.status_code}),
+            r.status_code)
