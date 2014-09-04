038ca42e87a678e3df6d94b9bef51ab1381b61db
Mat Wall <mat.wall@digital.cabinet-office.gov.uk>
2014-09-04 16:03:50 +0000

    Modified for new dev-env

--- environment.sh
+++ environment.sh
@@ -1,4 +1,3 @@
-export SETTINGS='config.DevelopmentConfig'
 export SYSTEMOFRECORD_URL='http://0.0.0.0:8000'
 export REDIS_URL="redis://user:@localhost:6379"
 export REDIS_NS_QUEUE_MINT="lr:queue:mint:"
