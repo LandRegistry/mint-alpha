789496ca0a2671de8e07b4ccdbf968cccd0b8d05
Juan Matthys Uys <opyate@gmail.com>
2014-10-02 16:21:51 +0100

    validating before sending to sor

--- themint/server.py
+++ themint/server.py
@@ -3,7 +3,7 @@ import json
 
 from themint import app
 from themint.service import message_service
-
+from datatypes.core import str_to_uni_dict
 
 @app.route('/', methods=['GET'])
 def index():
@@ -13,13 +13,20 @@ def index():
 @app.route('/titles/<title_number>', methods=['POST'])
 def post(title_number):
     app.logger.info("Received title number [%s] to mint new record with json: %s" % (title_number, request.json))
-    # TODO validate
-    message_service.wrap_message_for_system_of_record(request.json)
+    try:
+        message_service.wrap_message_for_system_of_record(str_to_uni_dict(request.json))
 
-    # TODO if validation fails, return 4XX
-    return make_response(
-        json.dumps({
-            'message': 'OK',
-            'status_code': 201 
-        }),
-        201)
+        return make_response(
+            json.dumps({
+                'message': 'OK',
+                'status_code': 201 
+            }),
+            201)
+    except Exception as e:
+        app.logger.error('Error when minting new: %s' % e)
+        return make_response(
+            json.dumps({
+                'message': 'Error',
+                'status_code': 400
+            }),
+            400)
--- themint/service/mint_message_service.py
+++ themint/service/mint_message_service.py
@@ -1,21 +1,26 @@
 from themint import app
 from themint.utils import unixts
-
+from datatypes import system_of_record_request_validator
+from datatypes.core import str_to_uni_dict
 
 class MintMessageService(object):
     def __init__(self, writer):
         self.writer = writer
 
     def wrap_message_for_system_of_record(self, message):
-        signed = {
-            "data": message,
-            "object_id": message['title_number'],
-            "initial_request_timestamp": unixts()
-            # optional:
-            # created_by
-            # reason_for_change
-            # chains  [ chain_name, chain_value ]
-        }
+        signed = str_to_uni_dict({
+            'object' : {
+                "data": message,
+                "object_id": message['title_number'],
+                "initial_request_timestamp": unixts()
+                # optional:
+                # created_by
+                # reason_for_change
+                # chains  [ chain_name, chain_value ]
+            }
+        })
+
+        system_of_record_request_validator.validate(signed)
 
         app.logger.info("Submitting %s to the system or record" % signed)
         self.writer.send_to_system_of_record(signed)
